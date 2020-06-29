0d834b1329ffd250b708376d2b5d1ba3aad6780d
Santiago Roman <santroma1@gmail.com>
2020-06-29 09:11:43 -0500

    routes for signup, login, and todolist

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { TodoListComponent } from './components/todo-list/todo-list.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { TodoItemComponent } from './components/todo-item/todo-item.component';
 import { TodoFormComponent } from './components/todo-form/todo-form.component';
+import { LoginComponent } from './components/login/login.component';
+import { SignupComponent } from './components/signup/signup.component';
+
+const routes:Routes = [
+    {
+        path: '',
+        redirectTo:'/login',
+        pathMatch:'full'
+    },
+    {
+        path:"login",
+        component:LoginComponent
+    },
+    {
+        path:"signup",
+        component:SignupComponent
+    },
+    {
+        path:"todolist",
+        component:TodoListComponent
+    }
+];
+
 
 @NgModule({
   declarations: [
@@ -14,12 +38,15 @@ import { TodoFormComponent } from './components/todo-form/todo-form.component';
     TodoListComponent,
     NavbarComponent,
     TodoItemComponent,
-    TodoFormComponent
+    TodoFormComponent,
+    LoginComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
     ReactiveFormsModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/signup/signup.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+
+@Component({
+  selector: 'app-signup',
+  templateUrl: './signup.component.html',
+  styleUrls: ['./signup.component.css']
+})
+export class SignupComponent implements OnInit {
+
+  constructor(  private fb:FormBuilder) { }
+
+  ngOnInit(): void {
+  }
+
+}
